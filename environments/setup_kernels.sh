5237f36951693e06bbfb6e876f73d4a7a215f47a
jre <s1137711@student.hsleiden.nl>
2026-05-12 18:47:54 +0200

    first commit

---
+++ environments/setup_kernels.sh
@@ -0,0 +1,5 @@
+conda activate spatial_main
+python -m ipykernel install --user --name=spatial_main --display-name "Python (spatial_main)"
+
+conda activate miso_env
+python -m ipykernel install --user --name=miso --display-name "miso"
